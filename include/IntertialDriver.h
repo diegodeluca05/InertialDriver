134756f5147d1e73aea2fac1e87aa708542d3dac
diegodeluca05 <152077831+diegodeluca05@users.noreply.github.com>
2025-11-22 19:11:29 +0100

    Refactor InertialDriver to use myVector for storage

--- include/IntertialDriver.h
+++ include/IntertialDriver.h
@@ -1,26 +1,17 @@
-#include<iostream>
+#include "myVector.h"
+#include "lettura.h"
+#include "misura.h"
+#include <iostream>
+    const int dimbuffer=2;
 
 class InertialDriver {
     private:
-        lettura misura[17];
-        static const int BUFFER_DIM=10; //numero a caso da cambiare
-        int puntantore=0; //puntatore che riporta la prima posizione libera 
+    myVector<Misura> first;
 
     public:
-        void push_back(const lettura (&arr)[17] ){ //accetta un array stile C contenente una misura e la memorizza nel buffer 
-
-        }
-        lettura* pop_front(){ // fornisce in output un array stile C contenente la misura più vecchia e la rimuove dal buffer; (restituisce un riferimento alla prima posizone dell'array)
-
-
-        }
-        void clear_buffet(){ // elimina (senza restituirle) tutte le misure salvate;
-
-        }
-        lettura get_reading(int num) { //accetta un numero tra 0 e 16 e ritorna la corrispondente lettura della misura più recente, senza cancellarla dal buffer;
-
-        }
-
-
+    class Invalid {};
+    explicit InertialDriver() : first(dimbuffer) {};
+    
+    void print(); 
 
 };
