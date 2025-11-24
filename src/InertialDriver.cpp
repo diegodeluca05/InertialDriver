43502d1bd62fae119bed2ba0ac0428b15e44bc5c
Ez10052 <eziomontemurro@outlook.com>
2025-11-24 20:22:35 +0100

    Update InertialDriver.cpp

--- src/InertialDriver.cpp
+++ src/InertialDriver.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include "lettura.h"
-#include "myVector.h"
-#include "misura.h"
+#include "../include/myVector.h"
+#include "../include/lettura.h"
+#include "../include/misura.h"
 using namespace std;
 
 class InertialDriver {
@@ -60,10 +60,40 @@ class InertialDriver {
 
 
 
-        lettura get_reading(int num) { //accetta un numero tra 0 e 16 e ritorna la corrispondente lettura della misura più recente, senza cancellarla dal buffer;
+        Lettura get_reading(int num) { //accetta un numero tra 0 e 16 e ritorna la corrispondente lettura della misura più recente, senza cancellarla dal buffer;
+            Misura misura = buffer.at(count);
+            Lettura lettura = misura[num];
+            return lettura;
 
         }
 
+        // Definizione dell'operatore << fuori dalla classe
+friend ostream& operator<<(ostream& os, const InertialDriver& driver) {
+    if (driver.count == 0) {
+        os << "Buffer vuoto." << endl;
+        return os;
+    }
 
+    // Indice dell'ultima misura salvata
+    int last_index = (driver.pnttr - 1 + driver.BUFFER_DIM) % driver.BUFFER_DIM;
 
-};
+    // Recupera l'ultima misura dal buffer
+    const Misura& last_misura = driver.buffer.at(last_index);
+
+    os << "Ultima misura salvata:" << endl;
+
+    // Cicla su tutti i sensori (17 sensori)
+    for (int i = 0; i < 17; i++) {
+        const Lettura& lettura = last_misura[i];
+        os << "Sensore " << i + 1 << ": "
+           << "Yaw_v: " << lettura.getYawV() << ", "
+           << "Yaw_a: " << lettura.getYawA() << ", "
+           << "Pitch_v: " << lettura.getPitchV() << ", "
+           << "Pitch_a: " << lettura.getPitchA() << ", "
+           << "Roll_v: " << lettura.getRollV() << ", "
+           << "Roll_a: " << lettura.getPitchA()<< endl;
+    }
+    return os;
+}
+
+    };
