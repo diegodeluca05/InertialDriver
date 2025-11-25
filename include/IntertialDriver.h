02e57925c5f73739be10b9a51c21b55ee611c3e4
diegodeluca05 <152077831+diegodeluca05@users.noreply.github.com>
2025-11-25 15:17:17 +0100

    Refactor InertialDriver class member variables

--- include/IntertialDriver.h
+++ include/IntertialDriver.h
@@ -1,37 +1,33 @@
+// ...existing code...
 #ifndef INERTIALDRIVER_H
 #define INERTIALDRIVER_H
 
-#include <iostream>
 #include "myVector.h"
 #include "lettura.h"
 #include "misura.h"
+#include <iostream>
+
+constexpr int dimbuffer = 2;
 
 class InertialDriver {
 private:
-    static const int BUFFER_DIM = 2;
-
-    int pnttr;       // Puntatore alla prossima posizione libera
-    int count;       // Numero di elementi attualmente nel buffer
-    myVector<Misura> buffer;
+    myVector<Misura> buffer;   // buffer circolare delle misure
+    int pnttr;                 // indice del primo spazio libero (tail)
+    int count;                 // numero di elementi attualmente presenti
 
 public:
     class Invalid {};
 
-    // Costruttore
     explicit InertialDriver();
 
-    // Aggiunge una misura nel buffer
-    void push_back(const Misura mis);
-
-    // Rimuove la misura più vecchia
+    void print();
     Misura pop_front();
-
-    // Svuota il buffer
+    void push_back(const Misura mis);
     void clear_buffer();
-
-    Lettura get_reading(int num);
+    Lettura get_reading(int num) const;
 
     friend std::ostream& operator<<(std::ostream& os, const InertialDriver& driver);
 };
 
-#endif
+#endif // INERTIALDRIVER_H
+// ...existing code...
